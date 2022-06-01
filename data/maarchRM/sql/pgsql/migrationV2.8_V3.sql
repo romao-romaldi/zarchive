0715574d817cf6492ec37dcb825d835a3ec8447a
Charlotte Bataille <charlotte.bataille@xelians.fr>
2022-06-01 12:05:10 +0200

    feat/17809 : rename migration file for release

--- data/maarchRM/sql/pgsql/migrationV2.8_V3.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "lifeCycle"."eventFormat" ("type","format","message","notification") VALUES
-('recordsManagement/completenessCheck', 'lastCheckedResId lastCheckedResCreated repositoryReference resourcesToCheck checkedResources failed timeout timeoutError', 'Contrôle d''exhaustivité des ressources', false);
-
-INSERT INTO "batchProcessing"."scheduling" ("schedulingId","name","taskId","frequency","parameters","executedBy","lastExecution","nextExecution","status") VALUES
-('completeness', 'Exhaustivité', '14', '00;08;;;;;;;', '30 3600', 'System', NULL, NULL, 'paused');
