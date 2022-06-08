53285cc0fc899fdbbbd40ad318b83224005e83dd
Charlotte Bataille <charlotte.bataille@xelians.fr>
2022-06-08 10:36:04 +0200

    update migrationV2.8_V2.9 for release

--- data/maarchRM/sql/pgsql/migrationV2.8_V2.9.sql
+++ data/maarchRM/sql/pgsql/migrationV2.8_V2.9.sql
@@ -1,9 +1,6 @@
 INSERT INTO "lifeCycle"."eventFormat" ("type","format","message","notification") VALUES
 ('recordsManagement/completenessCheck', 'lastCheckedResId lastCheckedResCreated repositoryReference resourcesToCheck checkedResources failed timeout timeoutError', 'Contrôle d''exhaustivité des ressources', false);
 
-INSERT INTO "batchProcessing"."scheduling" ("schedulingId","name","taskId","frequency","parameters","executedBy","lastExecution","nextExecution","status") VALUES
-('completeness', 'Exhaustivité', '14', '00;08;;;;;;;', '30 3600', 'System', NULL, NULL, 'paused');
-
 CREATE TABLE "digitalResource"."format" 
 (
     "puid" text NOT NULL,
